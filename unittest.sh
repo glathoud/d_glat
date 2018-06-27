1e241c3ec0bfe721652842390d411b82b2c3fea3
Guillaume Lathoud <guillaume.lathoud@outdooractive.com>
2018-06-27 15:23:50 +0200

    simplified unit testing

--- unittest.sh
+++ unittest.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
 
 set -e
 
@@ -7,5 +9,10 @@ B=$A/..
 
 MODULE_NAME=$(basename $A)
 
+
+FILE_LIST=$( find . -name '*.d' -exec grep -l unittest {} \; )
+IMPORT_LIST=$( ( sed 's:/:.:g' | sed 's:^\.\.::g' | sed "s/^\(.*\).d$/import ${MODULE_NAME}.\1;/g" ) <<< "$FILE_LIST" )
+echo $IMPORT_LIST
+
 cd $B
-rdmd -debug -unittest --eval="import ${MODULE_NAME}.unittest_import; import std.stdio; writeln; writeln( \"${MODULE_NAME}: All unittests passed\" )"
+rdmd --force -inline -debug -unittest --eval="${IMPORT_LIST}; import std.stdio; writeln; writeln( \"${MODULE_NAME}: All unittests passed\" )"
--- unittest_import.d
+++
@@ -1,3 +0,0 @@
-module d_glat_common.unittest_import;
-
-import d_glat_common.core_assoc_array;
