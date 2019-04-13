35878156a4cd2e5c756d45e9412d814e15a458cd
glathoud <glat@glat.info>
2019-04-13 19:07:10 +0200

    mean_cov of subset of data

--- flatmatrix/lib_stat.d
+++ flatmatrix/lib_stat.d
@@ -159,6 +159,126 @@ pure nothrow @safe @nogc
 }
 
 
+
+void mean_cov_inplace_dim( bool unbiased = true, T )
+  ( in ref MatrixT!T m
+    , in size_t[] subset
+    , ref MatrixT!T m_mean
+    , ref MatrixT!T m_cov )
+pure nothrow @safe
+{
+  pragma( inline, true );
+
+  m_mean.setDim( [ 1UL ] ~ m.dim[ 1..$ ] );
+  m_cov .setDim( [ m.restdim ] ~ m.dim[ 1..$ ] );
+  mean_cov_inplace!( unbiased, T )( m, subset, m_mean, m_cov );
+}
+
+void mean_cov_inplace( bool unbiased = true, T )
+  ( in ref MatrixT!T m
+    , in size_t[] subset
+    , ref MatrixT!T m_mean
+    , ref MatrixT!T m_cov )
+pure nothrow @safe @nogc
+{
+  pragma( inline, true );
+
+  debug
+    {
+      assert( m_mean.dim[ 0 ] == 1 );
+      assert( m_mean.dim[ 1..$ ] == m.dim[ 1..$ ] );
+
+      assert( m_cov.dim[ 0 ] == m.restdim );
+      assert( m_cov.dim[ 1..$ ] == m.dim[ 1..$ ] );
+
+      foreach (i; subset)
+        assert( 0 <= i  &&  i < m.dim[ 0 ] );
+    }
+
+  immutable n  = m.data.length;
+  immutable nv = subset.length;
+  immutable rd = m.restdim;
+
+  debug assert( n > 1 );
+
+  auto m_data    = m.data;
+  auto mean_data = m_mean.data;
+  auto cov_data  = m_cov.data;
+
+  mean_data[] = cast( T )( 0.0 );
+  cov_data[]  = cast( T )( 0.0 );
+
+  foreach (iss; subset)
+    {
+      size_t         im = iss * rd;
+      immutable next_im = im + rd;
+
+      size_t i_mean  = 0;
+      size_t off_cov = 0;
+      
+      while (im < next_im)
+        {
+          immutable vi = m_data[ im ];
+          
+          mean_data[ i_mean ] += vi;
+
+          {
+            size_t jm = im;
+            foreach (k; i_mean..rd)
+              cov_data[ off_cov + k ] += vi * m_data[ jm++ ];
+
+            debug assert( jm == next_im );
+          }
+          
+          ++im;
+          ++i_mean;
+          off_cov += rd;
+        }
+
+      debug assert( im == next_im );
+      debug assert( i_mean == rd );
+      debug assert( off_cov == cov_data.length );
+    }
+
+  immutable double nv_dbl = cast( double )( nv );
+  mean_data[] /= nv_dbl;
+  
+  immutable double r_cov = mixin((){
+      if (unbiased)
+        return `1.0 / (nv_dbl - 1.0)`;
+      else
+        return `1.0 / nv_dbl`;
+    }());
+
+  size_t offset = 0;
+  foreach (i; 0..rd)
+    {
+      offset += i;
+      
+      foreach (j; i..rd)
+        {
+          debug assert( offset == i * rd + j );
+          
+          auto x
+            = cov_data[ offset ]
+            = r_cov *
+            (cov_data[ offset ]
+             - nv_dbl *  mean_data[ i ] * mean_data[ j ]);
+          
+          if (i < j)
+            cov_data[ j * rd + i ] = x;
+          
+          ++offset;
+        }
+    }
+  debug assert( offset == rd*rd );
+}
+
+
+
+
+
+
 void mean_var_inplace_dim
 ( bool unbiased = true, T )
   ( in ref MatrixT!T m
@@ -284,6 +404,77 @@ unittest // ------------------------------
     Matrix m_mean, m_cov;
 
     mean_cov_inplace_dim( m, m_mean, m_cov );
+
+    /*
+      octave
+
+m = [ 9.123,    543.543, 234.2,  34.213;
+1.231,   -4.435, 5.4353, 7.56867;
+-3.54,   3543.534, 21.2134, 9.123;
+-10.432, -3.432, 25.543, 80.345;
++1.42,   +654.45, -32.432, -123.432;
++78.432, +12.123, -123.5435, -87.43
+];
+
+sprintf("%.12g ", mean(m))
+# 12.7056666667 790.963833333 21.7360333333 -13.2687216667
+
+sprintf("%.12g ",cov(m))
+# 1078.21878907 -13194.6422333 -1918.22091097 -1314.01354014 -13194.6422333 1905362.54113 15295.8135786 6349.01473539 -1918.22091097 15295.8135786 13892.3471641 5366.92317258 -1314.01354014 6349.01473539 5366.92317258 5917.89439291
+*/
+    
+    immutable double[] mean_truth =
+      [ 12.705666666666666, 790.9638333333332, 21.736033333333335, -13.26872166666667 ];
+    
+    immutable double[] cov_truth = [1078.21878907, -13194.6422333, -1918.22091097, -1314.01354014, -13194.6422333, 1905362.54113, 15295.8135786, 6349.01473539, -1918.22091097, 15295.8135786, 13892.3471641, 5366.92317258, -1314.01354014, 6349.01473539, 5366.92317258, 5917.89439291 ];
+
+    if (verbose)
+      {
+        writeln( "m_mean.data ", m_mean.data );
+        writeln( "mean_truth: ", mean_truth );
+      }
+    
+    assert( approxEqual( m_mean.data, mean_truth, 1e-10, 1e-10 ) );
+
+    if (verbose)
+      {
+        writeln( "m_cov.data: ", m_cov.data );
+        writeln( "cov_truth:  ", cov_truth);
+      }
+    
+    assert( approxEqual( m_cov .data,  cov_truth, 1e-10, 1e-10 ) );
+  }
+
+  
+  
+  {
+    if (verbose)
+      writeln("---------- Test mean_cov with subset");
+
+    auto nan = double.nan;
+    auto m = Matrix( [ 0, 4 ]
+                     , [ nan, nan, nan, nan,
+                         nan, nan, nan, nan,
+                         9.123,    543.543, 234.2,  34.213,
+                         nan, nan, nan, nan,
+                         1.231,   -4.435, 5.4353, 7.56867,
+                         nan, nan, nan, nan,
+                         nan, nan, nan, nan,
+                         -3.54,   3543.534, 21.2134, 9.123,
+                         -10.432, -3.432, 25.543, 80.345,
+                         nan, nan, nan, nan,
+                         nan, nan, nan, nan,
+                         +1.42,   +654.45, -32.432, -123.432,
+                         nan, nan, nan, nan,
+                         +78.432, +12.123, -123.5435, -87.43,
+                         nan, nan, nan, nan,
+                         nan, nan, nan, nan,
+                         ] );
+    immutable size_t[] subset = [ 2, 4, 7, 8, 11, 13 ];
+    
+    Matrix m_mean, m_cov;
+
+    mean_cov_inplace_dim( m, subset, m_mean, m_cov );
 
     /*
       octave
