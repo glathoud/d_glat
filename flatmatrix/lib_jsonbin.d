9f72a103593b10881969ea9cd71c2594778d3bd9
glathoud <glat@glat.info>
2019-07-16 19:14:08 +0200

    first full version

--- flatmatrix/lib_jsonbin.d
+++ flatmatrix/lib_jsonbin.d
@@ -5,9 +5,11 @@ public import d_glat.flatmatrix.core_matrix;
 import std.algorithm;
 import std.bitmanip;
 import std.conv;
+import std.exception : enforce;
 import std.file;
 import std.json;
 import std.range;
+import std.string : strip;
 import std.system;
 
 alias JsonBin = JsonBinT!double;
@@ -17,34 +19,64 @@ struct JsonBinT( T )
   string    j_str;
   MatrixT!T m;
 
+  // --- API: methods
+  
   bool isEmpty() const @safe pure nothrow
   {
     return 0 == m.data.length;
   }
 
   
-  JSONValue j()
+  JSONValue j() @safe pure 
   {
-  if (!_j_str_parsed)
-    _j = parseJSON( j_str );
+    if (!_j_str_parsed)
+      _j = parseJSON( j_str );
   
-  return _j;
-}
+    return _j;
+  }
   
-  ubyte[] toUbytes() const
+  ubyte[] toUbytes() const pure @trusted
     {
-  auto app = appender!(ubyte[]);
+      auto app = appender!(ubyte[]);
 
-  app.put( cast( ubyte[] )( j_str ) );
-  app.put( '\n' );
-  app.put( cast( ubyte[] )( to!string(m.dim) ) );
-  while( !(0 == (app.data.length+1) % double.sizeof) )
-    app.put( ' ' );
+      app.put( cast( ubyte[] )( j_str.dup ) );
+      app.put( '\n' );
+      app.put( cast( ubyte[] )( (T.stringof~':'~to!string(m.dim)).dup ) );
+      while( !(0 == (app.data.length+1) % T.sizeof) )
+        app.put( ' ' );
   
-  app.put( '\n' );
+      app.put( '\n' );
 
-  return app.data;
-}
+      // We always save the data in littleEndian format
+  
+      auto m_data = m.data;
+
+      if (endian == Endian.littleEndian)
+        {
+          app.put( cast( ubyte[] )( m_data.dup ));
+        }
+      else
+        {
+          ubyte[] ubytes = new ubyte[ m_data.length * (T.sizeof) ];
+          size_t index = 0;
+          foreach (d; m_data)
+            ubytes.write!(T, Endian.littleEndian)( d, &index );
+
+          app.put( ubytes );
+        }
+  
+      return app.data;
+    }
+
+  // --- API: Operators overloading
+
+  bool opEquals( in JsonBinT!T other ) const pure nothrow @safe @nogc
+  {
+    pragma( inline, true );
+
+    return this.j_str == other.j_str
+      &&  this.m == other.m;
+  }
 
   
  private:
@@ -52,37 +84,50 @@ struct JsonBinT( T )
   JSONValue _j;
 };
 
-JsonBinT!T jsonbin_of_filename( T )( in string filename )
+JsonBinT!T jsonbin_of_filename( T = double )( in string filename )
 {
   pragma( inline, true );
-  return jsonbin_of_filename!T( cast( char[] )( std.file.read( filename ) ) );
+  return jsonbin_of_chars!T( cast( char[] )( std.file.read( filename ) ) );
 }
 
-JsonBinT!T jsonbin_of_filename( T )( in char[] cdata )
+JsonBinT!T jsonbin_of_ubytes( T = double )( in ubyte[] cdata ) pure
+{
+  pragma( inline, true );
+  return jsonbin_of_chars!T( cast( char[] )( cdata ) );
+}
+
+
+JsonBinT!T jsonbin_of_chars( T = double )( in char[] cdata ) pure
 {
   immutable i     = cdata.countUntil( '\n' );
   immutable j_str = cdata[ 0..i ].idup;
   auto rest_0     = cdata[ i+1..$ ];
 
   immutable j_0   = rest_0.countUntil( '\n' );
-  immutable s_dim = rest_0[ 0..j_0 ].idup;
+  const     s_arr = rest_0[ 0..j_0 ].split( ':' );
+  immutable s_T   = s_arr[ 0 ].idup;
+  enforce( s_T == T.stringof );
+  
+  immutable s_dim = s_arr[ 1 ].idup;
   auto rest       = cast( ubyte[] )( rest_0[ j_0+1..$ ] );
   
-  auto dim = to!(size_t[])( s_dim );
+  auto dim = to!(size_t[])( s_dim.strip );
   
   T[] data;
-  
+
+  // We always saved the data in littleEndian format
+
   if (endian == Endian.littleEndian)
     {
       data = cast( T[] )( rest );
     }
   else
     {
-      immutable n = rest.length / double.sizeof;
+      immutable n = rest.length / T.sizeof;
       data = new T[ n ];
       size_t index;
       foreach (k; 0..n)
-        data[ k ] = rest.peek!(double, Endian.littleEndian)( &index );
+        data[ k ] = rest.peek!(T, Endian.littleEndian)( &index );
     }
   
   auto m = MatrixT!T( dim, data );
@@ -90,7 +135,88 @@ JsonBinT!T jsonbin_of_filename( T )( in char[] cdata )
   return JsonBinT!T( j_str, m );      
 }
  
- void jsonbin_write_to_filename( in JsonBin jb, in string filename )
- {
+void jsonbin_write_to_filename( in JsonBin jb, in string filename )
+{
   std.file.write( filename, jb.toUbytes );
+}
+
+
+
+unittest  // ------------------------------
+{
+  import std.stdio;
+
+  writeln;
+  writeln( "unittest starts: "~__FILE__ );
+
+  immutable verbose = false;
+
+  import std.algorithm;
+  import std.file;
+  import std.math;
+  import std.path;
+
+  immutable string tmp_filename = __FILE__~".tmpfile4unittest.jsonbin";
+
+  if (exists( tmp_filename ))
+    std.file.remove( tmp_filename );
+  
+  {
+    immutable string j_str = `{abcd:1234,efgh:{xyz:"qrst"}}`;
+    auto m = Matrix( [ 0, 3 ]
+                     , [ 1.234, 2.3456, 20.123,
+                         17.123, -12.3, 0.0,
+                         5.0, 6.0, 7.0,
+                         8.0, 9.0, 11.0,
+                         -12.34, +2.65, -123.456
+                         ]
+                     );
+    const jb0 = JsonBin( j_str, m );
+
+    const ubytes = jb0.toUbytes();
+
+    auto jb1 = jsonbin_of_ubytes( ubytes );
+
+    assert( jb0.j_str == jb1.j_str );
+    assert( jb0.m == jb1.m );
+
+    if (verbose)
+      {
+        writeln( jb0 );
+      }
+
+  }
+  
+  
+  {
+    immutable string j_str = `{abcd:1234,efgh:{xyz:"qrst"}}`;
+    auto m = Matrix( [ 0, 3 ]
+                     , [ 1.234, 2.3456, 20.123,
+                         17.123, -12.3, 0.0,
+                         5.0, 6.0, 7.0,
+                         8.0, 9.0, 11.0,
+                         -12.34, +2.65, -123.456
+                         ]
+                     );
+    const jb0 = JsonBin( j_str, m );
+
+    jsonbin_write_to_filename( jb0, tmp_filename );
+    
+    auto jb1 = jsonbin_of_filename( tmp_filename );
+
+    assert( jb0.j_str == jb1.j_str );
+    assert( jb0.m == jb1.m );
+
+    if (verbose)
+      {
+        writeln( jb0 );
+      }
+
+  }
+
+  if (exists( tmp_filename ))
+    std.file.remove( tmp_filename );
+  
+  
+  writeln( "unittest passed: "~__FILE__ );
 }
