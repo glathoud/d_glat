4d72a6c50d52146face0b5a90784ad014c348525
glathoud <glat@glat.info>
2020-02-26 18:37:09 +0100

    getStackTrace getMemUsage printMemUsage

---
+++ core_runtime.d
@@ -0,0 +1,58 @@
+module d_glat.core_runtime;
+
+import core.memory : GC;
+import core.runtime : defaultTraceHandler;
+import core.thread : getpid;
+import std.array : appender, join;
+import std.conv : to;
+import std.exception : enforce;
+import std.process : executeShell;
+import std.stdio : writeln, stdout;
+import std.string : strip;
+
+string getStackTrace()
+// Code from ARSD
+// https://forum.dlang.org/post/aenumslnnxeedlrkwhaz@forum.dlang.org
+{
+	version(Posix) {
+		// druntime cuts out the first few functions on the trace as they are internal
+		// so we'll make some dummy functions here so our actual info doesn't get cut
+		Throwable.TraceInfo f5() { return defaultTraceHandler(); }
+		Throwable.TraceInfo f4() { return f5(); }
+		Throwable.TraceInfo f3() { return f4(); }
+		Throwable.TraceInfo f2() { return f3(); }
+		auto stuff = f2();
+	} else {
+		auto stuff = defaultTraceHandler();
+	}
+
+	return stuff.toString();
+}
+
+void printMemUsage()
+{
+  writeln(getMemUsage());
+  stdout.flush;
+}
+
+string getMemUsage()
+{
+  auto app = appender!(string[]);
+  
+  immutable pid = getpid();
+  {
+    auto x = executeShell( "cat /proc/"~to!string(pid)~"/status  | grep VmHWM" );
+    enforce( 0 == x.status );
+    app.put(x.output.strip);
+  }
+  {
+    auto x = executeShell( "cat /proc/"~to!string(pid)~"/status  | grep VmRSS" );
+    enforce( 0 == x.status );
+    app.put(x.output.strip);
+  }
+  auto stats = GC.stats; 
+  app.put( "stats.usedSize: "~to!string( stats.usedSize )~", stats.freeSize: "~to!string( stats.freeSize ));
+  app.put( "" );
+
+  return app.data.join( '\n' );
+}
