c76f15cc037965b0d8bb0facb4c91858d37888f4
glathoud <glat@glat.info>
2022-01-31 09:06:08 +0300

    faster gzip

--- core_gzip.d
+++ core_gzip.d
@@ -1,14 +1,24 @@
 module d_glat.core_gzip;
 
+import d_oa_common.core_unittest;
+import d_oa_common.lib_oa_tmpfilename;
+import std.array;
+import std.conv;
+import std.file;
+import std.process;
+import std.range;
+import std.stdio;
 import std.zlib;
 
 ubyte[] gunzip( in ubyte[] data )
-@trusted
 {
+  auto app = appender!(ubyte[]);
+
   auto U = new UnCompress( HeaderFormat.gzip );
-  auto d1 = cast( ubyte[] )( U.uncompress( data ) );
-  auto d2 = cast( ubyte[] )( U.flush() );
-  return d1 ~ d2;
+  app.put( cast( ubyte[] )( U.uncompress( data ) ) );
+  app.put( cast( ubyte[] )( U.flush() ) );
+  
+  return app.data;
   
   // Old code below
   // 
@@ -22,7 +32,6 @@ ubyte[] gunzip( in ubyte[] data )
 }
 
 ubyte[] gunzip( in void[] data )
-@trusted
 {
   return gunzip( cast( ubyte[] )( data ) );
 }
@@ -30,17 +39,106 @@ ubyte[] gunzip( in void[] data )
 
 // Consider using `std.string.representation` in some use cases.
 
+private bool _tried_shell = false, _use_shell = false, _checking_stability = false;
+
+class _GzipLock {}; shared auto _gzipLock = new _GzipLock;
+
 ubyte[] gzip( in ubyte[] data )
-@trusted
 {
-  auto       C = new Compress( 9, HeaderFormat.gzip );
-  auto  gzip_1 = cast( ubyte[] )( C.compress( data ) );
-  auto  gzip_2 = cast( ubyte[] )( C.flush() );
-  return gzip_1 ~ gzip_2;
+  immutable MIN_LENGTH_FOR_SHELL_GZIP = 100 * 1024L;
+  
+  if (MIN_LENGTH_FOR_SHELL_GZIP <= data.length  &&  (_use_shell  ||  !_tried_shell))
+    {
+      if (!_tried_shell  &&  !_checking_stability)
+        {
+          _checking_stability = true;
+          
+          // First time, check as well that always the same output
+          // (useful for hashing etc.). Normally the '-n' option of
+          // gzip should be enough for that, but checking is safer.
+
+          immutable s = "A la claire fontaine, m'en allant promener, j'ai trouve l'eau si claire, que je m'y suis baigne.";
+          
+          const d_1 = cast(ubyte[])( s.replicate( 2 + (MIN_LENGTH_FOR_SHELL_GZIP / s.length) ) );
+          mixin(_asrt!`MIN_LENGTH_FOR_SHELL_GZIP < d_1.length`);
+
+          const out_1 = gzip( d_1 );
+
+          if (_tried_shell  &&  _use_shell)
+            {
+              const d_2 = cast(ubyte[])( s.replicate( 2 + (MIN_LENGTH_FOR_SHELL_GZIP / s.length) ) );
+              mixin(_asrt!`MIN_LENGTH_FOR_SHELL_GZIP < d_2.length`);
+              
+              const out_2 = gzip( d_2 );
+
+              _tried_shell = true;
+              _use_shell   = out_1 == out_2;
+              if (!_use_shell)
+                {
+                  stderr.writeln( "For some reason, gzip() output non deterministic - even if we passed the '-n' option. Falling back onto the (likely slower) D implementation." );
+                  stderr.flush;
+                }
+            }
+
+          _checking_stability = false;
+        }
+
+      // Tried with pipeShell, but blocking on big files, hence the
+      // present solution using a temporary file.
+      if (_tried_shell  &&  _use_shell  ||  _checking_stability)
+        {
+      try
+        {
+          immutable tmpfn = get_tmpfilename( "d_oa_common.core_gzip" );
+          std.file.write( tmpfn, data );
+
+          // -n important: do not save the tmpfn into the file, to try to guarantee always same output
+          auto tmp = executeShell("gzip -n \""~tmpfn~"\"");
+
+          // Detect errors.
+          if (0 != tmp.status)
+            {
+              throw new Exception ( "d_oa_common.core_gzip.gzip: error returned by the shell. Falling back onto slower D implementation. Error caught: " ~ to!string(tmp.output) );
+            }
+
+          // Read output
+          auto outfn = tmpfn~".gz";
+          auto ret = cast(ubyte[])( std.file.read( outfn ) );
+          std.file.remove( outfn );
+
+          synchronized( _gzipLock )
+            {
+              if (!_tried_shell)
+                {
+                  _tried_shell = true;
+                  _use_shell   = true;
+                }
+            }
+
+          return ret; // Success. Done!
+        }
+      catch ( Throwable t )
+        {
+          stderr.writeln( "d_oa_common.core_gzip.gzip failed to use the shell. Falling back onto slower D implementation.");
+          stderr.flush;
+          synchronized( _gzipLock )
+            {
+              _tried_shell = true;
+              _use_shell   = false;
+            }
+        }
+    }
+    }
+  
+  auto app = appender!(ubyte[]);
+  auto   C = new Compress( 9, HeaderFormat.gzip );
+  app.put( cast( ubyte[] )( C.compress( data ) ) );
+  app.put( cast( ubyte[] )( C.flush() ) );
+
+  return app.data;
 }
 
 ubyte[] gzip( void[] data )
-@trusted
 {
   return gzip( cast( ubyte[] )( data ) );
 }
