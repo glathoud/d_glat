abf7c2041f463bcdf147f6658dd7cb3dd85ffd56
glathoud <glat@glat.info>
2022-08-10 09:02:05 +0300

    to manipulate strings to/from octave

---
+++ core_octave.d
@@ -0,0 +1,36 @@
+module d_glat.core_octave;
+
+/*
+  Tools to manipulate strings of data to and from Octave
+  (MATLAB-like).
+
+  The Boost License applies, see file ./LICENSE
+
+  By Guillaume Lathoud, 2022
+  glat@glat.info
+ */
+
+import d_glat.lib_json;
+import std.algorithm : map;
+import std.array : array;
+import std.json;
+import std.string : replace;
+
+string mstr_of_arr(T/*typically float or double*/)( in T[] arr )
+{
+  return JSONValue( arr ).toString( JSONOptions.specialFloatLiterals ).replace( `"NaN"`, "nan" ).replace( `"Infinite"`, "Inf" ).replace( `"-Infinite"`, "-Inf" );
+}
+
+T[] arr_of_mstr(T = double/*typically float or double*/)( in string s )
+{
+  immutable tmp = s.replace("-nan","\"NaN\"").replace("nan","\"NaN\"")
+    .replace("-Inf","\"-Infinite\"").replace("Inf","\"Infinite\"");
+
+  import std.stdio;
+  writeln("xxx ____ tmp: ", tmp);
+  
+  return parseJSON( tmp
+                    , -1
+                    , JSONOptions.specialFloatLiterals
+                    ).array.map!((a) => cast(T)( json_get_double( a ))).array;
+}
