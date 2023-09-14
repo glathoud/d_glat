a864aa1a0ffd6097699f9c615b7af815feaa4c0f
glathoud <glat@glat.info>
2023-09-14 07:38:20 +0300

    multiloopC

---
+++ lib_multiloop.d
@@ -0,0 +1,37 @@
+module d_glat.lib_multiloop;
+
+import std.algorithm : map;
+import std.array : join, replicate;
+
+/*
+  Generate code for multiple, imbricated, foreach loops for a set of
+  parameters `paramname_arr` and their respective possible values
+  `v_arr_of_paramname[ paramname ]`.
+  
+  The innermost loop calls `fname( ...paramvalue_arr... )`.
+
+  Example:
+
+  mixin(multiloopC(["a","b","c"],"v_arr_of_pn","f"));
+
+  is equivalent to:
+
+  foreach (a; v_arr_of_pn["a"])
+    foreach (b; v_arr_of_pn["b"])
+      foreach (c; v_arr_of_pn["c"])
+        f(a,b,c);
+  
+  By Guillaume Lathoud, 2023
+  glat@glat.info
+
+  The Boost license applies to this file, as described in ./LICENSE
+ */
+
+string multiloopC( in string[] /*to loop:*/paramname_arr, in string v_arr_of_paramname, in string fname ) pure
+{
+  return (paramname_arr
+          .map!((pn) => "foreach (ref "~pn~"; "~v_arr_of_paramname~"[\""~pn~"\"]){")
+          .join("")
+          )~fname~"("~(paramname_arr.join(','))~");"
+    ~replicate("}",paramname_arr.length);
+}
