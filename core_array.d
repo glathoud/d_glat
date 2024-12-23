84a8a652a52378c48c8d2a2740880715e08b6bb8
glathoud <glat@glat.info>
2024-12-23 18:53:51 +0300

    arr_each_parallel_C  arr_filter_inplace(_C)

--- core_array.d
+++ core_array.d
@@ -5,6 +5,7 @@ import d_glat.core_assert;
 import d_glat.core_string : _tli;
 import std.algorithm : sort;
 import std.array : appender, array;
+import std.exception : assumeUnique;
 import std.conv : to;
 import std.format : format;
 import std.math : abs, isNaN;
@@ -44,11 +45,37 @@ void arr_change_order_inplace_nogc(T)( in size_t[] ind_arr, ref T[] buff, ref T[
 }
 
 
+string arr_each_parallel_C
+(string taskPool_c = "taskPool"
+ , string a_c = "a", string a_step_c="a_step", string a_begin_c="a_begin", string a_end_c="a_end"
+ , string arr_len_c="arr_len")
+ ( in string arr_c, in string do_c ) pure nothrow @safe 
+{
+  return mixin(_tli!q{
+      {
+        immutable ${arr_len_c} = (${arr_c}).length;
+        immutable ${a_step_c} = cast(size_t)( ceil( (cast(double)( ${arr_len_c} ))
+                                                      / (cast(double)( 1 + (${taskPool_c}).size ))));
+
+        foreach (${a_begin_c}; (${taskPool_c}).parallel( iota( 0, ${arr_len_c}, ${a_step_c} )
+                                                         , /*workUnitSize:*/1 ))
+          {
+            immutable ${a_end_c} = min( ${arr_len_c}, ${a_begin_c} + ${a_step_c} );
+            foreach (${a_c}; ${a_begin_c}..${a_end_c})
+              {
+                ${do_c}
+              }
+          }
+      }
+    });
+}
+
+
 
 alias arr_ensure_length = ensure_length;
 
 T[] ensure_length(T)( size_t desired_length, ref T[] arr )
-pure nothrow @safe
+  pure nothrow @safe
 /*
   Typical usages:
   
@@ -110,8 +137,53 @@ bool arr_equal_nan(T)( in T[] a, in T[] b )
 }
 
 
+void arr_filter_inplace(string test_c, T)( ref T[] arr ) pure nothrow @safe
+// In practice, should probably be @nogc since `arr.length` can only diminish.
+{
+  mixin(arr_filter_inplace_C("arr",test_c));
+}
 
 
+string arr_filter_inplace_C(string a_c="a", string b_c="b",string old_length_c="old_length")
+  ( in string arr_c, in string test_c ) pure nothrow @safe
+// Code for single pass, (GC & CPU)-cost-effective inplace removal of
+// several elements of an array.
+//
+// In practice, should probably be @nogc since `arr.length` can only diminish.
+{
+  return mixin(_tli!q{
+      {
+        immutable size_t ${old_length_c} = ${arr_c}.length;
+
+        size_t ${a_c} = 0;
+        size_t ${b_c} = 0;
+
+        /* ${a_c} == ${b_c} */
+        for (;
+             (${test_c})  &&  ${a_c} < ${old_length_c};
+             ++${a_c}, ++${b_c}
+             )
+          {/*keep the element, nothing to change*/}
+        
+        /*At this point, either finished, or ${b_c} < ${a_c}*/
+        for (;
+             ${a_c} < ${old_length_c};
+             ++${a_c})
+          {
+            if (!(${test_c}))
+              continue; /*skip*/
+
+            /*keep*/
+            ${arr_c}[ ${b_c}++ ] = ${arr_c}[ ${a_c} ];
+          }
+
+        ${arr_c}.length = ${b_c};
+      }
+    });
+}
+
+
+  
 string arr_fold_C(string opt_acctype_c="double")
   ( in string opt_accinit_c, in string loopspec_c, in string iter_c ) pure nothrow @safe
 {
@@ -328,8 +400,12 @@ T[] uniq_of_sorted_arr(T)( in T[] arr ) pure
 
 unittest
 {
+  import std.algorithm;
   import std.path;
+  import std.range;
   import std.stdio;
+
+  enum verbose = true;
   
   writeln;
   writeln( "unittest starts: ", baseName( __FILE__ ) );
@@ -384,6 +460,25 @@ unittest
 
   }
 
+  {
+    enum test_c = "0 == (a % 3)";
+    auto arr_0 = assumeUnique( iota( 100 ).array );
+    auto arr_1 = assumeUnique( arr_0.filter!test_c.array );
+
+    auto arr_2 = arr_0.dup;
+    arr_filter_inplace!test_c( arr_2 );
+
+    if (verbose)
+      {
+        writeln( "arr_0: ", arr_0 );
+        writeln( "arr_1: ", arr_1 );
+        writeln( "arr_2: ", arr_2 );
+      }
+
+    assert( arr_2 == arr_1 );
+  }
+
+  
   {
     size_t[] arr;
     arr_set_iota( 7, arr );
