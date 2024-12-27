802b02042b3c60717055a766ee56083f95c961b4
glathoud <glat@glat.info>
2024-12-27 13:50:29 +0300

    FixedPermBigFirst

--- lib_math_fixedperm.d
+++ lib_math_fixedperm.d
@@ -32,9 +32,15 @@ import std.conv : to;
   The Boost license applies to this file, as described in ./LICENSE
 */
 
-alias FixedPerm = FixedPermT!size_t;
+alias FixedPerm         = FixedPermT!size_t;
+alias FixedPermInfinite = FixedPermT!(size_t, true);
 
-struct FixedPermT( T, bool infinite = false, string i_out_init = "0" )
+alias FixedPermBigFirst         = FixedPermT!(size_t, false, true);
+alias FixedPermBigFirstInfinite = FixedPermT!(size_t, true, true);
+
+struct FixedPermT( T, bool infinite = false, bool big_first = false )
+// Typical behaviour (big_first=false): the very first few numbers are
+// small.  To start with big numbers right away, try big_first=true.
 {
   immutable T n;
 
@@ -58,7 +64,21 @@ struct FixedPermT( T, bool infinite = false, string i_out_init = "0" )
 
     next_pow_2 = (n == prev_pow_2)  ?  n  :  (prev_pow_2 << 1);
 
-    step = 1;
+    i_in = 0;
+
+    static if (!big_first)
+      {
+        step_init = 1;
+        i_out_init = 0;
+      }
+    else
+      {
+        step_init = next_pow_2 - 1;
+        i_out_init = n-1;
+      }
+
+    step  = step_init;
+    i_out = i_out_init;
   }
   
 
@@ -78,8 +98,16 @@ struct FixedPermT( T, bool infinite = false, string i_out_init = "0" )
 
   void popFront() pure @safe @nogc {
     do {
-      i_out = (i_out + step) % next_pow_2;
-      step  = (1 + step) % next_pow_2;
+      static if (!big_first)
+        {
+          i_out = (i_out + step) % next_pow_2;
+          step  = (1 + step) % next_pow_2;
+        }
+      else
+        {
+          i_out = (i_out - step + next_pow_2) % next_pow_2;
+          step  = (-1 + step + next_pow_2) % next_pow_2;
+        }
     } while (i_out >= n);
     ++i_in;
 
@@ -87,8 +115,8 @@ struct FixedPermT( T, bool infinite = false, string i_out_init = "0" )
       {
         if (0 == (i_in % n))
           {
-            i_out = mixin(i_out_init);
-            step = 1;
+            i_out = i_out_init;
+            step  = step_init;
           }
       }
   }
@@ -122,8 +150,8 @@ struct FixedPermT( T, bool infinite = false, string i_out_init = "0" )
   private:
 
   immutable T next_pow_2;
-  T i_in = 0, step = 1, i_out = mixin(i_out_init);
-
+  immutable T step_init, i_out_init;
+  T i_in, step, i_out;
 }
 
 unittest
@@ -134,7 +162,7 @@ unittest
   writeln;
   writeln( "unittest starts: ", baseName( __FILE__ ) );
 
-  immutable verbose = false ;
+  immutable verbose = false;
 
   import std.algorithm;
   import std.range;
@@ -164,31 +192,51 @@ unittest
         }
     }
 
-    if (0 < N)
+    static foreach (BIG_FIRST; [false,true])
     {
-      auto rfp = FixedPermT!(size_t, /*infinite:*/true)( N );
+      {
+        if (verbose)
+          {
+            writeln;
+            writeln("N:", N, ", BIG_FIRST: ", BIG_FIRST);
+          }
 
-      assert(!rfp.empty);
+        if (0 < N)
+          {
+            auto rfp = FixedPermT!(size_t, /*infinite:*/true, BIG_FIRST)( N );
+          
+            assert(!rfp.empty);
+          
+            const arr = rfp.take( 3*N ).array;
+          
+            assert(!rfp.empty);
+          
+            if (verbose)
+              {
+                if (N < 6)
+                  {
+                    writeln("(infinite) N ", N);
+                    writeln("(infinite) arr ", arr);
+                  }
+                else
+                  {
+                    scope const arrN = arr[0..N];
+                    writeln("(infinite) N ", N);
+                    writeln("(infinite) arrN[0..min($,30)] ", arr[0..min($,30)]);
+                    writeln("(infinite) arrN[($>>1)..min($,($>>1)+30)] ", arr[($>>1)..min($,($>>1)+30)]);
+                    writeln("(infinite) arrN[($-min($,30))..$] ", arr[($-min($,30))..$]);
+                    
+                  }
 
-      const arr = rfp.take( 3*N ).array;
-
-      assert(!rfp.empty);
-
-      if (verbose)
-        {
-          if (N < 6)
-            {
-              writeln("(infinite) N ", N);
-              writeln("(infinite) arr ", arr);
-            }
-        }
-      
-      assert( arr[0..N] == arr[N..(2*N)] );
-      assert( arr[(2*N)..(3*N)] == arr[N..(2*N)] );
-
-      assert( arr[0..N].dup.sort.array == iota(N).array ); 
+              }
+          
+            assert( arr[0..N] == arr[N..(2*N)] );
+            assert( arr[(2*N)..(3*N)] == arr[N..(2*N)] );
+          
+            assert( arr[0..N].dup.sort.array == iota(N).array ); 
+          }
+      }
     }
-
     
     {
       auto rfp = FixedPerm( N );
@@ -233,7 +281,8 @@ unittest
           assert( v == arr[ i ] );
         }
     }
-    
+
+
     if (verbose)
       {
         writeln( "end test_one: ", N );
